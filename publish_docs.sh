ee1bd2e022812ba61490673516d0b19c91788023
Zhonglei Qiu <zhongleiqiu@gmail.com>
2015-04-05 13:29:02 +0800

    publish docs

---
+++ publish_docs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+dir=`dirname $0`
+
+# 生成文档
+cd $dir
+npm run docs
+
+cd "$dir/docs"
+
+if [[ ! -d ./.git ]]; then
+  git init
+  git remote add origin git@github.com:qiu8310/smart-npm.git
+  git co -b gh-pages
+else
+  git co gh-pages -f
+fi
+
+git add . -A
+git commit -m "publish docs"
+#git push origin gh-pages --force
+#
+#cd $dir
+
+
+
+
+
